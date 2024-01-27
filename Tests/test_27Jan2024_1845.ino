def1652da1166907f3d12d477020f3fb6e51a803
Julia Steiwer <42389995+JuliaSteiwer@users.noreply.github.com>
2024-01-27 19:12:26 +0100

    Combine programmable Ve pins with temperature sensor data.

--- Tests/test_27Jan2024_1845.ino
+++ Tests/test_27Jan2024_1845.ino
@@ -2,7 +2,7 @@
 //Sensors connected:
 // -- DS18B20 temperature sensor. -> data is being send, the values are correct
 // Currently powered via USB cable.
-// Set isTxConfirmed = false
+// Set isTxConfirmed = false 
 // Ve pins are activated and working (3.3V power supply for sensors) 
 
 #include "LoRaWan_APP.h"
