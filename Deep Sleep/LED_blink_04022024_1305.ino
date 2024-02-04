0dbb954e097f91c86f87b14eb768de78b0726e20
Julia Steiwer <42389995+JuliaSteiwer@users.noreply.github.com>
2024-02-04 13:07:37 +0100

    Turn LED on when awake; LED is on during deep sleep

---
+++ Deep Sleep/LED_blink_04022024_1305.ino
@@ -0,0 +1,57 @@
+// LED is supposed to be off during deep sleep (60sec) and turn on when board wakes up
+// in reality, the LED is on during deep sleep and off when the board wakes up
+
+#define GPIO 46
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
+void setup() {
+  pinMode(GPIO, OUTPUT);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  digitalWrite(GPIO, HIGH);
+
+  Serial.println("Hello World!");
+
+  digitalWrite(GPIO, LOW); // turn that enable pin OFF -> 0V
+  //rtc_gpio_isolate(GPIO); // prevent pin from stealing power
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
